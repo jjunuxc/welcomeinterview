b597dc43e541d55e039313c07c00acd14dd66526
jjunuxc <chanjunyin199@gmail.com>
2025-02-24 15:37:55 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+#!/bin/python
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/reset")
+def reset():
+    return render_template("reset.html")
+
+
